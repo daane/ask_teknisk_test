a2457071c28dfbe57c1486e78d5fda2da2e9ad62
David Aanestad <davidaan@stud.ntnu.no>
2021-04-15 22:00:56 +0200

    Route test

--- tests/Controller/MakerControllerTest.php
+++ tests/Controller/MakerControllerTest.php
@@ -7,6 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class MakerControllerTest extends WebTestCase
 {
 
+    public function testMakerPing()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/ping');
+        $this->assertResponseIsSuccessful();
+        $responseData = json_decode($client->getResponse()->getContent());
+        $this->assertTrue(is_numeric($responseData));
+        // Assumes that the number of "Maker" entities is known:
+        $this->assertEquals(20,$responseData);
+        // I am not quite sure how to write tests that interacts with the DB,
+        // but I can look more into it if it's important
+    }
 
     public function testListOfMakersIsOk(): void
     {
